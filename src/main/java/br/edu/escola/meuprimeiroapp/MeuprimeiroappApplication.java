5550f1d7e8dba154ad345a3d0621daba3972a40d
Sthefany <sthefany.7531362@aluno.mg.gov.br>
2026-09-14 15:41:34 -0300

    chore: cria projeto Spring Boot

---
+++ src/main/java/br/edu/escola/meuprimeiroapp/MeuprimeiroappApplication.java
@@ -0,0 +1,13 @@
+package br.edu.escola.meuprimeiroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuprimeiroappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuprimeiroappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/escola/meuprimeiroapp/MeuprimeiroappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.escola.meuprimeiroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuprimeiroappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
